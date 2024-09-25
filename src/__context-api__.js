fd50bb4631405457d43c1e47d3d4020721dc6902
rootnure <nur.diu.2791@gmail.com>
2024-09-26 01:17:40 +0600

    Context API

---
+++ src/__context-api__.js
@@ -0,0 +1,8 @@
+/**
+ * ----- USE CONTEXT API TO AVOID MULTI LEVEL PROPS DRILLING ------
+ * 1. Create a context
+ * 1.5. Export it
+ * 2. Add provider for the context with a value
+ * 2.5. value could be anything
+ * 3. useContext() hook to access value in the context API
+ */

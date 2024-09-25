5fa427abcc7ba2bc8a5897b1d810cab247d44ba1
rootnure <nur.diu.2791@gmail.com>
2024-09-26 00:18:51 +0600

    Reusable form for Sign UP and Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
-import HookForm from './components/HookForm/HookForm'
+import ReusableForm from './components/ReusableForm/ReusableForm'
+// import HookForm from './components/HookForm/HookForm'
 // import RefForm from './components/RefForm/RefForm'
 // import SimpleForm from './components/SimpleForm/SimpleForm'
 // import StatefulForm from './components/StatefulForm/StatefulForm'
 
 function App() {
+
+  const handleSignUp = data => {
+    console.log('Sign Up Form', data);
+  }
+
+  const handleUpdate = data => {
+    console.log('Update form', data);
+  }
 
   return (
     <>
@@ -12,7 +21,23 @@ function App() {
       {/* <SimpleForm></SimpleForm> */}
       {/* <StatefulForm></StatefulForm> */}
       {/* <RefForm></RefForm> */}
-      <HookForm></HookForm>
+      {/* <HookForm></HookForm> */}
+      <ReusableForm
+        handleSubmit={handleSignUp}
+      >
+        <div>
+          <p>Sign up now to explore the unknown</p>
+        </div>
+      </ReusableForm>
+      <ReusableForm
+        handleSubmit={handleUpdate}
+        submitBtnText={'Update'}
+        formTitle={'Update'}
+      >
+        <div>
+          <p>Always keep your profile updated</p>
+        </div>
+      </ReusableForm>
     </>
   )
 }
--- src/components/__form__.js
+++ src/components/__form__.js
@@ -7,4 +7,8 @@
  * 3. controlled element: use a single state (object) to handle all the input fields value
  * 
  * 4. uncontrolled element: useRef() hook to create reference to the element and access value by using like: nameRef.current.value
+ * 
+ * 5. use hook to handle states, submit and, error. i.e. react hook form
+ * 
+ * 6. Reusable component
  */

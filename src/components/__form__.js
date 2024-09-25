0b4196bb688528b494cdc7b7dfe4df0b786c26ab
rootnure <nur.diu.2791@gmail.com>
2024-09-25 23:43:14 +0600

    useRef() hook to handle form input field value

--- src/components/__form__.js
+++ src/components/__form__.js
@@ -2,5 +2,9 @@
  * 1. sue form tag and onSubmit handler with event (e)
  * 1.5. Form e access e.target.[name of the input field].value
  * 
- * 2. use individual field state
+ * 2. controlled element: use individual field state for each input field
+ * 
+ * 3. controlled element: use a single state (object) to handle all the input fields value
+ * 
+ * 4. uncontrolled element: useRef() hook to create reference to the element and access value by using like: nameRef.current.value
  */

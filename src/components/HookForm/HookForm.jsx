9a2e7bdfaa90637bed6057a7aa0f7fc6fad39908
rootnure <nur.diu.2791@gmail.com>
2024-09-25 23:57:16 +0600

    intro to custom hook. custom hook to handle code redundancy

---
+++ src/components/HookForm/HookForm.jsx
@@ -0,0 +1,38 @@
+import useInputState from "../../hooks/useInputState";
+
+const HookForm = () => {
+
+  // const [name, handleNameChange] = useInputState('Raaj');
+
+  const emailState = useInputState('Bura bbau')
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log('form data', emailState.value);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        {/* <input
+          value={name}
+          onChange={handleNameChange}
+          type="text"
+          name="name" /> */}
+        <br />
+        <input
+          {...emailState}
+          type="email"
+          name="email" />
+        <br />
+        <input
+          type="password"
+          name="password" />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
+
+export default HookForm;
